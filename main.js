9bb9bffa4d60b7c3ddf32945546401e5ee5defb9
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-11-11 18:22:52 -0500

    navigation scroll and click events updates

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const navlistitems = document.querySelectorAll(".nav-list-item>a");
 const login = document.getElementById("login");
 const signup = document.getElementById("signup");
+const mobbtn = document.getElementById('span-container')
+const mobwrap = document.getElementById('mob-wrapper')
+const spanclasses = ['top-span','mid-span','bottom-span']
+const spans = [...mobbtn.children]
 let interval;
 // iterate over nav list items and execute event listeners
 navlistitems.forEach((item, index) => {
@@ -35,3 +39,64 @@ function hoverLeaveListItem(e) {
   }
 }
 
+// test toggle nav by class
+const toggleNav = (nav) => {
+  nav.classList.toggle('hi-nav')
+  const navclosed = document.querySelector('#nav').classList['value'].split` `.filter(x=>/(hi-nav|bye-nav)/i.test(x)).length < 2
+  // conditions based on screen size and scroll position
+  if(window.scrollY > 0 && window.innerWidth <= 989){
+   if(!navclosed){
+    spans.forEach(sp=>sp.classList.remove('invert-span'))
+   } else {
+    spans.forEach(sp=>sp.classList.add('invert-span'))
+  
+   }
+  }
+  }
+  const removeNav = (nav) => {
+      nav.classList.remove('hi-nav')
+  }
+  const revertSpan = (span) => {
+      let classes = [...span.classList].filter(x=>x!='mob-span'&&x!='no-pointer')
+      console.log(classes)
+      if(classes.length == 1) span.classList.remove(classes[0])
+  }
+  
+  // test click mob button
+  mobbtn.onclick = e => {
+      console.log('target acquired')
+      spans.forEach((sp,idx)=>{
+         sp.classList.toggle(spanclasses[idx])
+         toggleNav(document.getElementById('nav'))
+      })
+  }
+  
+  // resize window
+  window.onresize = e => {
+    // toggle button classes are removed
+    if(window.innerWidth > 989){
+      removeNav(document.getElementById('nav'))
+      spans.forEach(sp=> revertSpan(sp))
+    }
+  }
+
+// window onscroll
+window.onscroll = e => {
+  console.log(window.scrollY)
+  if(window.scrollY > 0 && window.innerWidth <= 989){
+    invertHeader()
+  } else {
+    removeInvertHeader()
+  }
+}
+
+function invertHeader(){
+  mobwrap.classList.add('invert-env')
+  spans.forEach(sp=>sp.classList.add('invert-span'))
+  document.getElementById('logo').classList.add('invert-logo')
+}
+function removeInvertHeader(){
+  mobwrap.classList.remove('invert-env')
+  spans.forEach(sp=>sp.classList.remove('invert-span'))
+  document.getElementById('logo').classList.remove('invert-logo')
+}
--- tests.js
+++ tests.js
@@ -24,37 +24,3 @@
 // }
 
 
-// test toggle nav by class
-const toggleNav = (nav) => {
-nav.classList.toggle('hi-nav')
-}
-const removeNav = (nav) => {
-    nav.classList.remove('hi-nav')
-}
-const revertSpan = (span) => {
-    let classes = [...span.classList].filter(x=>x!='mob-span'&&x!='no-pointer')
-    console.log(classes)
-    if(classes.length == 1) span.classList.remove(classes[0])
-}
-
-// test click mob button
-const mobbtn = document.getElementById('span-container')
-const spanclasses = ['top-span','mid-span','bottom-span']
-const spans = [...mobbtn.children]
-mobbtn.onclick = e => {
-    console.log('target acquired')
-    spans.forEach((sp,idx)=>{
-       sp.classList.toggle(spanclasses[idx])
-       toggleNav(document.getElementById('nav'))
-    })
-}
-
-// resize window
-window.onresize = e => {
-  // toggle button classes are removed
-  if(window.innerWidth > 989){
-    removeNav(document.getElementById('nav'))
-    spans.forEach(sp=> revertSpan(sp))
-  }
-
-}
